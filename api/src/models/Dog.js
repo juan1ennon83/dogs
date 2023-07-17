ab72baeaf669ab771601382f7441d3f257621e25
juan1ennon83 <juanmasalros@gmail.com>
2023-07-17 03:39:29 -0500

    doggy model

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,16 +1,33 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
+  sequelize.define('dog',{        //my dog model creation based on PI request parameters
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false, 
+    },
+    image: {
+      type: DataTypes.TEXT, //verificar si es STRING
       allowNull: false,
     },
-    
       name: {
         type: DataTypes.STRING,
         allowNull: false,
-  });
+        unique: true,
+  },
+      height: {
+        type: DataTypes.FLOAT, //VERIFY ALSO WEIGHT
+      allowNull: false,
+  },
+      weight: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+  },
+      life_span: {
+        type: DataTypes.INTEGER,
+      allowNull: false,
+  }}
+    );
 };
