ae6cb786f095d6efafb79df7b95a4f16ec376f89
lennon <lennon@penguin>
2023-07-26 22:06:45 -0500

    Backend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import axios from 'axios';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { LandingPages, Home, Create, DetailDog } from './views';
+import NavBar from './components/NavBar/NavBar.jsx';
+
+axios.defaults.baseURL = 'http://localhost:3001';
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      { location.pathname !== '/' && <NavBar/> }
+      <Routes>
+        <Route path='/' element={ <LandingPages/> } />
+        <Route path='/home' element={ <Home/> } />
+        <Route path='/create' element={ <Create/> } />
+        <Route path='/home/:id' element={ <DetailDog/> } />
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from './redux/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById( 'root' );
+const root = createRoot( rootElement );
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
